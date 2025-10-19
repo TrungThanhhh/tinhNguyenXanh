957f7183159e952185bc011cd78bc6f4c3ffc0ac
TrungThanhhh <trungthanh070104@gmail.com>
2025-10-19 19:52:25 +0700

    Add project files.

---
+++ TinhNguyenXanh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TinhNguyenXanh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
